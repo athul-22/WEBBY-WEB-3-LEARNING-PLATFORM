aaa02d2035258a94a5cd01cd3f0f19caf8c2b6c8
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-02-10 21:44:56 +0530

    V1

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 function mainFun(){
     //ELEMENTS
     var dashboard = document.getElementById("dashboard");
-    
-    var mainDB = localStorage.getItem(mainDB);
+    var signin_button = document.getElementsByClassName("signin-button");
+
+    var mainDB = localStorage.getItem("nameDB");
+
     console.log(mainDB);
 
-    // SIGNIN / DASHBOARD TEXT CHANGE
+    // SIGNIN - DASHBOARD TEXT CHANGE
     if(mainDB == null){
         dashboard.innerHTML = "SIGNIN";
+         // SIGNIN BUTTON CLICK TO POPUP
+    $('#dashboard').on('click', function() {
+        $(".custom-model-main-1").addClass('model-open-1');
+         }); 
+       // PRIMARY BUTTON CLICK TO POPUP 
+       $('.primary').on('click', function() {
+        $(".custom-model-main-1").addClass('model-open-1');
+         }); 
+       //  CLOSING BUTTTON 
+       $(".close-btn-1, .bg-overlay-1").click(function(){
+       $(".custom-model-main-1").removeClass('model-open-1');
+        });
     }
     else{
-        dashboard.innerHTML = "DASHBOARD"
+        dashboard.innerHTML = "DASHBOARD";
+        window.open("dashboard.html")
     }
+}
+
+    if(mainDB != null){
+
+    }
+
+// SIGNIN DATA STORE FUNCTION
+
+function storeData(){
+    var name_value = document.getElementById("name").value;
+    var email_value = document.getElementById("email").value;
+
+    localStorage.setItem("nameDB", name_value);  
+    localStorage.setItem("emailDB", email_value);  
+
+    var n = localStorage.getItem("nameDB");
+
+
+    console.log(n);
+    console.log(emailDB);
+
 }
