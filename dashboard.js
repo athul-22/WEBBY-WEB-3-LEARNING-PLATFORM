ad38445fd46b5ab8156efa95bd94a8fab1f53a55
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-02-11 07:44:15 +0530

    V3

--- dashboard.js
+++ dashboard.js
@@ -2,10 +2,45 @@ function dashLoad(){
     var name_value = localStorage.getItem("nameDB");
     var email_value = localStorage.getItem("emailDB");
 
-    
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
 
-    console.log(name);
-    console.log(email);
+    name.innerHTML = name_value;
+    email.innerHTML = email_value;
+
+    console.log(name_value);
+    console.log(email_value);
+
+    // GETTING PROFILE LOCALSTORAGE
+
+ 
+    var profile_DB = localStorage.getItem("profileDB");
+
+    console.log(localStorage.getItem("profileDB"));
+    console.log(profile_DB);
 
 
+    if(profile_DB == "man"){
+        document.getElementsByClassName("profile_img").src = "MAN_1.png";
+    }else{
+        document.getElementsByClassName("profile_img").src = 'WOMEN_1.png';
+    }
+}
+
+
+function manFun(){
+    var profile_img = document.getElementById("profile_img");
+    var man = document.getElementById("man");
+    var profileDB = localStorage.setItem("profileDB", "man");
+
+    document.getElementsByClassName("profile_img").src = 'MAN_1.png';
+
+}
+
+function womenFun(){
+    var profile_img = document.getElementById("profile_img");
+    var man = document.getElementById("women");
+    var profileDB = localStorage.setItem("profileDB", "women")
+
+    document.getElementsByClassName("profile_img").src = 'WOMEN_1.png';
 }
