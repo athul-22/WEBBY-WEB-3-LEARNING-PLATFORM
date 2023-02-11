d5db2cec90a6cfa384623ed329f682eab615153c
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-02-11 06:49:49 +0530

    V2

---
+++ dashboard.js
@@ -0,0 +1,11 @@
+function dashLoad(){
+    var name_value = localStorage.getItem("nameDB");
+    var email_value = localStorage.getItem("emailDB");
+
+    
+
+    console.log(name);
+    console.log(email);
+
+
+}
