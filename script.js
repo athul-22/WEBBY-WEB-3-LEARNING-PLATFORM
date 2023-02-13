f5b2a6a30883a7dad1b600b69fbf4cf782c00d19
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-02-13 12:21:36 +0530

    V7

--- script.js
+++ script.js
@@ -3,28 +3,35 @@ function mainFun(){
     var dashboard = document.getElementById("dashboard");
     var signin_button = document.getElementsByClassName("signin-button");
     var mainDB = localStorage.getItem("nameDB");
-
+    var primary = document.getElementsByClassName("primary");
     console.log(mainDB);
 
     // SIGNIN - DASHBOARD TEXT CHANGE
     if(mainDB == null){
+
         dashboard.innerHTML = "SIGNIN";
+        $(".primary").html("SIGN IN");
+
             // SIGNIN BUTTON CLICK TO POPUP
             $('#dashboard').on('click', function() {
             $(".custom-model-main-1").addClass('model-open-1');
               }); 
+
             // PRIMARY BUTTON CLICK TO POPUP 
              $('.primary').on('click', function() {
             $(".custom-model-main-1").addClass('model-open-1');
               }); 
-            //  CLOSING BUTTTON 
+            // CLOSING BUTTTON 
              $(".close-btn-1, .bg-overlay-1").click(function(){
              $(".custom-model-main-1").removeClass('model-open-1');
             });
     }
     else{
         dashboard.innerHTML = "DASHBOARD"; 
-        //window.open("dashboard.html");
+        $('.primary').on('click', function(){
+          window.open("dashboard.html");
+        });
+        //
     }
 }
 
